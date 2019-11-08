df1cb631fd86d2754103fd5f030bda38c19fb4bb
avtrujillo <alexandervtrujillo@gmail.com>
2019-11-07 18:06:33 -0800

    remove devise before_filter in ApplicationsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :require_login
 
   protected
 
+=begin
   def configure_permitted_parameters
     attributes = [:name, :surname,:username, :email, :avatar]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
+=end
 
   def not_authenticated
     redirect_to '/sign_in', alert: 'Login required'
