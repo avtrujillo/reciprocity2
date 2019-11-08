1230e8b5d88a29b97bd525a3b908e4d81d5c3c0f
avtrujillo <alexandervtrujillo@gmail.com>
2019-11-07 18:11:39 -0800

    add omniauth login links to signin and signup pages

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,8 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook,
+           (ENV['TEST_FB_APP_ID'] || ENV['FB_APP_ID']),
+           callback_url: "#{ENV['ROOT_URL']}users/auth/facebook/callback"
+  # TODO: add other providers
 
+  on_failure {|env| OmniauthCallbacksController.action(:failure).call(env)}
+end
