ea1d4a95ec9ba4df4cc3cc28b6915f221fe377b8
avtrujillo <alexandervtrujillo@gmail.com>
2019-11-28 15:24:29 -0800

    change migration from devise to sorcery so that it uses BCrypt's built-in methods for password hash and salt manipulation

--- db/migrate/20191103070405_move_from_devise_to_sorcery.rb
+++ db/migrate/20191103070405_move_from_devise_to_sorcery.rb
@@ -1,16 +1,44 @@
 class MoveFromDeviseToSorcery < ActiveRecord::Migration[5.2]
+  # Sorcery stores password hashes and salts separately, while devise concatenates them together
+  # In addition to updating the schema,
+  # this migration ensures that current user passwords are not invalidated during the transition
 
   def up
     add_column :users, :reset_password_token_expires_at, :datetime
-    split_encrypted_password
+
+    add_column :users, :crypted_password, :string
+    add_column :users, :salt, :string
+
+    save_passwords_up
+
+    remove_column :users, :encrypted_password, :string
   end
 
   def down
-    remove_column :users, :reset_password_token_expires_at
+    add_column :users, :encrypted_password, :string
     concat_encrypted_password
+
+    remove_column :users, :crypted_password
+    remove_column :users, :salt
+
+    remove_column :users, :reset_password_token_expires_at
   end
 
   private
+
+  def save_passwords_up
+    User.all.each do |user|
+      raw_pw_hash = BCrypt::Password.new(user.encrypted_password)
+      user.update!(salt: raw_pw_hash.salt, crypted_password: raw_pw_hash.checksum)
+    end
+  end
+
+  def save_passwords_down
+    User.all.each do |user|
+      user.update!(encrypted_password: (user.salt + user.crypted_password))
+    end
+
+  end
 
   # https://www.freecodecamp.org/news/how-does-devise-keep-your-passwords-safe-d367f6e816eb/
 
