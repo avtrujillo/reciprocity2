aa55d6447d6f08c6df8b6bb639772967e3223875
avtrujillo <alexandervtrujillo@gmail.com>
2019-12-01 13:35:40 -0800

    set default sorcery salt to empty string

--- db/migrate/20191103070405_move_from_devise_to_sorcery.rb
+++ db/migrate/20191103070405_move_from_devise_to_sorcery.rb
@@ -3,6 +3,19 @@ class MoveFromDeviseToSorcery < ActiveRecord::Migration[5.2]
   # In addition to updating the schema,
   # this migration ensures that current user passwords are not invalidated during the transition
 
+  def change
+    add_column :users, :salt, :string, default: ''
+    # BCrypt (which is the default encryption provider for both Sorcery and Devise) already adds its own
+    # salt when hashing passwords, and appends it to the beginning of the :crypted_password field
+    # See https://www.freecodecamp.org/news/how-does-devise-keep-your-passwords-safe-d367f6e816eb/
+    # By leaving the :salt field as an empty string for existing users, we ensure that currently stored password hashes
+    # are not invalidated.
+    # https://github.com/Sorcery/sorcery/issues/217
+    rename_column :users, :encrypted_password, :crypted_password
+  end
+end
+
+=begin
   def up
     add_column :users, :reset_password_token_expires_at, :datetime
 
@@ -16,7 +29,8 @@ class MoveFromDeviseToSorcery < ActiveRecord::Migration[5.2]
 
   def down
     add_column :users, :encrypted_password, :string
-    concat_encrypted_password
+
+    save_passwords_down
 
     remove_column :users, :crypted_password
     remove_column :users, :salt
@@ -29,7 +43,8 @@ class MoveFromDeviseToSorcery < ActiveRecord::Migration[5.2]
   def save_passwords_up
     User.all.each do |user|
       raw_pw_hash = BCrypt::Password.new(user.encrypted_password)
-      user.update!(salt: raw_pw_hash.salt, crypted_password: raw_pw_hash.checksum)
+      user.assign_attributes(salt: raw_pw_hash.salt, crypted_password: raw_pw_hash.checksum)
+      user.save!(validate: false)
     end
   end
 
@@ -39,8 +54,6 @@ class MoveFromDeviseToSorcery < ActiveRecord::Migration[5.2]
     end
 
   end
-
-  # https://www.freecodecamp.org/news/how-does-devise-keep-your-passwords-safe-d367f6e816eb/
 
   def split_encrypted_password
     add_column :users, :crypted_password, :string
@@ -68,3 +81,4 @@ class MoveFromDeviseToSorcery < ActiveRecord::Migration[5.2]
   end
 
 end
+=end
